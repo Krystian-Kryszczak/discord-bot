df6be865e22c6c04c144e4430198acf19fb6af02
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-04-30 07:14:15 +0200

    Micronaut framework added.

---
+++ src/main/java/krystian/kryszczak/Application.java
@@ -0,0 +1,9 @@
+package krystian.kryszczak;
+
+import io.micronaut.runtime.Micronaut;
+
+public final class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
