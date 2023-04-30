c65669036ebd89bdd76a7c2e2a4d3ef9966c1da9
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-05-01 01:43:44 +0200

    Added OpenAiConfigurationTest.

---
+++ src/test/java/krystian/kryszczak/configuration/openai/OpenAiConfigurationTest.java
@@ -0,0 +1,32 @@
+package krystian.kryszczak.configuration.openai;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public final class OpenAiConfigurationTest {
+
+    @Test
+    public void tokenTest() {
+        Map<String, Object> items = new HashMap<>();
+        items.put("open-ai.token", "sk-test-token");
+        items.put("open-ai.gpt-model", "gpt-3.5-turbo");
+
+        ApplicationContext ctx = ApplicationContext.run(items);
+        OpenAiConfiguration openAiConfiguration = ctx.getBean(OpenAiConfiguration.class);
+
+        String token = openAiConfiguration.getToken();
+        String gptModel = openAiConfiguration.getGptModel();
+
+        assertEquals("sk-test-token", token);
+        assertEquals("gpt-3.5-turbo", gptModel);
+
+        ctx.close();
+    }
+}
