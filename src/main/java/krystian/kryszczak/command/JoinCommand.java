d0625eff3f8fe8212a68e1a0b2cf59597e9f9498
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-05-01 20:47:00 +0200

    Added "join" command.

---
+++ src/main/java/krystian/kryszczak/command/JoinCommand.java
@@ -0,0 +1,27 @@
+package krystian.kryszczak.command;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import jakarta.inject.Singleton;
+import krystian.kryszczak.model.lavaplayer.LavaPlayerAudioProvider;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+@Singleton
+public final class JoinCommand extends Command {
+    private final LavaPlayerAudioProvider provider;
+    JoinCommand(LavaPlayerAudioProvider provider) {
+        super("join");
+        this.provider = provider;
+    }
+
+    @Override
+    public Publisher<Void> execute(MessageCreateEvent event) {
+        return Mono.justOrEmpty(event.getMember())
+            .flatMap(Member::getVoiceState)
+            .flatMap(VoiceState::getChannel)
+            .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
+            .then();
+    }
+}
