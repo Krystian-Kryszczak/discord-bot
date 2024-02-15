1fd89f8e4074738e7d2820f7fd4ccb989631349f
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-15 21:54:09 +0100

    Update/refactor models tests.

--- src/test/java/krystian/kryszczak/discord/bot/model/elevenlabs/TextToSpeechTest.java
+++ src/test/java/krystian/kryszczak/discord/bot/model/elevenlabs/TextToSpeechTest.java
@@ -1,37 +1,38 @@
 package krystian.kryszczak.discord.bot.model.elevenlabs;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public final class TextToSpeechTest {
-
     @Test
-    void testVoiceSettingsToJson(ObjectMapper objectMapper) throws JsonProcessingException {
-        final String result = objectMapper.writeValueAsString(
-            new TextToSpeech("Hello world!", "TxGEqnHWrfWFTfGW9XjX", new VoiceSettings(0, 0))
-        );
+    public void testVoiceSettingsToJson(@NotNull ObjectMapper objectMapper) {
+        assertDoesNotThrow(() -> {
+            final String result = objectMapper.writeValueAsString(
+                new TextToSpeech("Hello world!", "TxGEqnHWrfWFTfGW9XjX", new VoiceSettings(0, 0))
+            );
 
-        final TextToSpeech textToSpeech = objectMapper.readValue(result, TextToSpeech.class);
+            final TextToSpeech textToSpeech = objectMapper.readValue(result, TextToSpeech.class);
 
-        assertNotNull(result);
-        assertEquals("Hello world!", textToSpeech.getText());
-        assertEquals("TxGEqnHWrfWFTfGW9XjX", textToSpeech.getModelId());
-        assertEquals(new VoiceSettings(0, 0), textToSpeech.getVoiceSettings());
+            assertNotNull(result);
+            assertEquals("Hello world!", textToSpeech.getText());
+            assertEquals("TxGEqnHWrfWFTfGW9XjX", textToSpeech.getModelId());
+            assertEquals(new VoiceSettings(0, 0), textToSpeech.getVoiceSettings());
+        });
     }
 
     @Test
-    void testVoiceSettingsToJsonEqualsPattern(ObjectMapper objectMapper) throws JsonProcessingException {
-        final String result = objectMapper.writeValueAsString(
-            new TextToSpeech("string", "eleven_monolingual_v1", new VoiceSettings(0, 0))
-        );
+    public void testVoiceSettingsToJsonEqualsPattern(@NotNull ObjectMapper objectMapper) {
+        assertDoesNotThrow(() -> {
+            final String result = objectMapper.writeValueAsString(
+                new TextToSpeech("string", "eleven_monolingual_v1", new VoiceSettings(0, 0))
+            );
 
-        final String pattern = """
+            final String pattern = """
             {
               "text": "string",
               "model_id": "eleven_monolingual_v1",
@@ -44,7 +45,8 @@ public final class TextToSpeechTest {
             .replaceAll("\n", "")
             .replaceAll(" ", "");
 
-        assertNotNull(result);
-        assertEquals(pattern, result);
+            assertNotNull(result);
+            assertEquals(pattern, result);
+        });
     }
 }
--- src/test/java/krystian/kryszczak/discord/bot/model/elevenlabs/VoiceSettingsTest.java
+++ src/test/java/krystian/kryszczak/discord/bot/model/elevenlabs/VoiceSettingsTest.java
@@ -1,24 +1,24 @@
 package krystian.kryszczak.discord.bot.model.elevenlabs;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public final class VoiceSettingsTest {
-
     @Test
-    void testVoiceSettingsToJson(ObjectMapper objectMapper) throws JsonProcessingException {
-        final String result = objectMapper.writeValueAsString(new VoiceSettings(0.5f, 1.0f));
+    public void testVoiceSettingsToJson(@NotNull ObjectMapper objectMapper) {
+        assertDoesNotThrow(() -> {
+            final String result = objectMapper.writeValueAsString(new VoiceSettings(0.5f, 1.0f));
 
-        final VoiceSettings voiceSettings = objectMapper.readValue(result, VoiceSettings.class);
+            final VoiceSettings voiceSettings = objectMapper.readValue(result, VoiceSettings.class);
 
-        assertNotNull(result);
-        assertEquals(0.5f, voiceSettings.getStability());
-        assertEquals(1.0f, voiceSettings.getSimilarityBoost());
+            assertNotNull(result);
+            assertEquals(0.5f, voiceSettings.getStability());
+            assertEquals(1.0f, voiceSettings.getSimilarityBoost());
+        });
     }
 }
