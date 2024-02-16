ae3f5a6fe5b8a07e82f6a8bd5c768ba730b3484a
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-16 02:05:23 +0100

    Create OpenAI model classes.

--- src/main/java/krystian/kryszczak/discord/bot/model/openai/ChatCompletion.java
+++ src/main/java/krystian/kryszczak/discord/bot/model/openai/ChatCompletion.java
@@ -1,4 +1,20 @@
 package krystian.kryszczak.discord.bot.model.openai;
 
-public class ChatCompletion {
+import io.micronaut.core.annotation.Introspected;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Introspected
+public record ChatCompletion(@NotNull String model, @NotNull Message @NotNull [] messages, boolean stream) {
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ChatCompletion other) {
+            return Objects.equals(this.model, other.model) &&
+                Arrays.equals(this.messages, other.messages) &&
+                this.stream == other.stream;
+        }
+        return false;
+    }
 }
--- src/main/java/krystian/kryszczak/discord/bot/model/openai/Message.java
+++ src/main/java/krystian/kryszczak/discord/bot/model/openai/Message.java
@@ -1,4 +1,7 @@
 package krystian.kryszczak.discord.bot.model.openai;
 
-public class Message {
-}
+import io.micronaut.core.annotation.Introspected;
+import jakarta.validation.constraints.NotNull;
+
+@Introspected
+public record Message(@NotNull String role, @NotNull String content) {}
