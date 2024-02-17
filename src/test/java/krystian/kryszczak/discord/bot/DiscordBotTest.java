b436f30ffdbf8ab49c65014d63312ba30449d370
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-17 02:03:37 +0100

    Update DiscordBot test.

--- src/test/java/krystian/kryszczak/discord/bot/DiscordBotTest.java
+++ src/test/java/krystian/kryszczak/discord/bot/DiscordBotTest.java
@@ -2,19 +2,17 @@ package krystian.kryszczak.discord.bot;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import jakarta.inject.Inject;
-
 @MicronautTest
-class DiscordBotTest {
-
-    @Inject
-    EmbeddedApplication<?> application;
+@RequiredArgsConstructor
+public final class DiscordBotTest {
+    private final EmbeddedApplication<?> application;
 
     @Test
-    void testItWorks() {
+    public void testItWorks() {
         Assertions.assertTrue(application.isRunning());
     }
 }
