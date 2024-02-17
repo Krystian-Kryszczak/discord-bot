c9a7acfe2cc2091f462b3994fab85e07d0295f5a
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2024-02-17 01:49:52 +0100

    Update declarative HTTP clients.

--- src/main/java/krystian/kryszczak/discord/bot/http/elevenlabs/ReactorElevenLabsHttpClient.java
+++ src/main/java/krystian/kryszczak/discord/bot/http/elevenlabs/ReactorElevenLabsHttpClient.java
@@ -1,24 +1,26 @@
 package krystian.kryszczak.discord.bot.http.elevenlabs;
 
-import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import krystian.kryszczak.discord.bot.model.elevenlabs.TextToSpeech;
 import reactor.core.publisher.Mono;
 
 @Client(id = "eleven-labs")
 @Header(name = "xi-api-key", value = "${eleven-labs.token}")
 public interface ReactorElevenLabsHttpClient {
-    @Post("/v1/text-to-speech/${eleven-labs.default-voice-id}")
+    @Post("/v1/text-to-speech/${eleven-labs.default-voice-id}?optimize_streaming_latency=3")
     Mono<HttpResponse<byte[]>> textToSpeech(@Body TextToSpeech body);
-    @Post("/v1/text-to-speech/{voice_id}")
-    Mono<HttpResponse<byte[]>> textToSpeech(@Body TextToSpeech body, @NonNull String voice_id);
+    @Post("/v1/text-to-speech/{voiceId}?optimize_streaming_latency={optimizeLatency}")
+    Mono<HttpResponse<byte[]>> textToSpeech(@Body TextToSpeech body, @NotNull String voiceId, @Min(0) @Max(4) int optimizeLatency);
 
-    @Post("/v1/text-to-speech/${eleven-labs.default-voice-id}/stream")
+    @Post("/v1/text-to-speech/${eleven-labs.default-voice-id}/stream?optimize_streaming_latency=3")
     Mono<HttpResponse<byte[]>> textToSpeechStream(@Body TextToSpeech body);
-    @Post("/v1/text-to-speech/{voice_id}/stream")
-    Mono<HttpResponse<byte[]>> textToSpeechStream(@Body TextToSpeech body, @NonNull String voice_id);
+    @Post("/v1/text-to-speech/{voiceId}/stream?optimize_streaming_latency={optimizeLatency}")
+    Mono<HttpResponse<byte[]>> textToSpeechStream(@Body TextToSpeech body, @NotNull String voiceId, @Min(0) @Max(4) int optimizeLatency);
 }
--- src/main/java/krystian/kryszczak/discord/bot/http/openai/ReactorOpenAIHttpClient.java
+++ src/main/java/krystian/kryszczak/discord/bot/http/openai/ReactorOpenAIHttpClient.java
@@ -6,6 +6,8 @@ import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.multipart.MultipartBody;
+import krystian.kryszczak.discord.bot.model.openai.completion.request.ChatCompletionRequestBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static io.micronaut.http.HttpHeaders.AUTHORIZATION;
@@ -15,4 +17,7 @@ import static io.micronaut.http.HttpHeaders.AUTHORIZATION;
 public interface ReactorOpenAIHttpClient {
     @Post(value = "/v1/audio/transcriptions", produces = MediaType.MULTIPART_FORM_DATA)
     Mono<String> createTranscription(@Body MultipartBody body);
+
+    @Post(value = "/v1/chat/completions", produces = MediaType.APPLICATION_JSON)
+    Flux<String> createChatCompletion(@Body ChatCompletionRequestBody body);
 }
